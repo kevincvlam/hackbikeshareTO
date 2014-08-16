83704adbd2c0b7298b4fdfceb242265e230a933c
kevincvlam <i.am.kev@gmail.com>
2014-08-16 13:19:48 -0400

    time to sync with ryan

--- flask_app.py
+++ flask_app.py
@@ -21,28 +21,45 @@ def api_help():
 Mock data
 '''
 tripDuration = {'minutes':20}
+station = {'id':1}
 
 '''
 # Services
 '''
-@app.route('/api/tripduration', methods = ['GET'])
-def tripduration():
+@app.route('/api/bikeduration', methods = ['GET'])
+def bikeDuration():
 	# Read in Arguments
 	start = request.args.get('start')
 	end = request.args.get('end')
 	weather = request.args.get('weather')
 	return jsonify(tripDuration)
 
-def walkduration():
+@app.route('/api/walkduration', methods = ['GET'])
+def walkDuration():
 	# Read in Arguments
 	start = request.args.get('start')
 	end = request.args.get('end')
 	return jsonify(tripDuration)
 
+@app.route('/api/closeststationwithbike', methods = ['GET'])
+def closestStationWithBike():
+	return jsonify(station)
+
+@app.route('/api/closeststationwithdock', methods = ['GET'])
+def closestStationWithDock():
+	return jsonify(station)
+
+@app.route('/api/bikepath', methods = ['GET'])
+def bikePath():
+	return 'NOT READY'
+
+@app.route('/api/walkpath', methods = ['GET'])
+def walkPath():
+	return 'NOT READY'
+
 '''
 # Example Code And Tests
 '''
-
 @app.route('/argtest', methods =['GET'])
 def argtest():
 	start = request.args.get('start')
