21f1d6c279f1e1aff142ca3020b9773ba2703a98
kevincvlam <i.am.kev@gmail.com>
2014-08-16 19:13:57 -0400

    Test Case working pretty much, using mock data

--- flask_app.py
+++ flask_app.py
@@ -63,8 +63,13 @@ def closestStation():
 	poi = request.args.get('poi')
 
 	# Return Mock Data for Now
-	mock = {'ID':7003, 'Name': 'Madison Ave / Bloor St W', 'Latitude': 43.667158, 'Longitude':-79.402761}
-	return jsonify(mock)
+	if("Bay" in poi):
+		mock = {'ID':7041, 'Name': 'Edward St / Yonge St', 'Latitude': 43.65702, 'Longitude':-79.382249}
+		return jsonify(mock)
+	else:
+		mock = {'ID':7003, 'Name': 'College St / Borden', 'Latitude': 43.657, 'Longitude':-79.4056}
+		return jsonify(mock)
+
     
     # Real Function
 	if poi is None:
